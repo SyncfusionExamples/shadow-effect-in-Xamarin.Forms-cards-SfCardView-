fe3916fd8ec6841b9fded9f0e767f764f6535a37
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-04-29 12:39:56 +0530

    Sample included

---
+++ Cards_Shadow/Cards_Shadow/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards_Shadow
+{
+    public class CardModel
+    {
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
---
+++ Cards_Shadow/Cards_Shadow/CardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards_Shadow
+{
+    public class CardViewModel
+    {
+        public IEnumerable<CardModel> Items { get; set; }
+        public CardViewModel()
+        {
+            Items = new CardModel[]
+            {
+                new CardModel(){ Title = "FaceBook" , Image = "FacebookFill.png"},
+                new CardModel(){ Title = "Gmail" , Image = "GmailFill.png"},
+                new CardModel(){ Title = "Instagram" , Image = "InstagramFill.png"},
+                new CardModel(){ Title = "Whatsapp" , Image = "WhatsappFill.png"},
+            };
+        }
+    }
+}
